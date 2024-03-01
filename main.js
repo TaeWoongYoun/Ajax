5afeae30a790bea4164782cc6b28bdfc82350f9b
xodnd0507 <24457545yong@gmail.com>
2024-03-01 23:08:22 +0900

    test : Get test

---
+++ main.js
@@ -0,0 +1,19 @@
+// get
+// $.get('https://codingapple1.github.io/hello.txt')
+// .done(function(data){
+//     console.log(data)
+// })
+// .fail(function(){
+//     console.log('실패함')
+// })
+
+// post
+// $.post('https://codingapple1.github.io/hello.txt', {name:'kim'})
+// .done(function(data){
+//     console.log(data)
+// })
+
+$.get('https://codingapple1.github.io/price.json')
+.done(function(price){
+    console.log(price.price)
+})
