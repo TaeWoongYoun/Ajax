b949e31177db1e624af90684ce8973116b1cd715
xodnd0507 <24457545yong@gmail.com>
2024-03-01 23:24:41 +0900

    chore : Create 3 card

--- main.js
+++ main.js
@@ -17,4 +17,21 @@
 // .done(function(price){
 //     console.log(price.price)
 // })
+var card = 
+`<div class="col-sm-4">
+<img src="https://via.placeholder.com/600" class="w-100">
+<h5>Card title</h5>
+<p>가격 : 70000</p>
+</div>`
 
+
+document.querySelectorAll('.row').forEach(function(row){
+    // row.innerHTML = card;
+    for(var i = 0; i < 3; i++){
+        row.innerHTML += card;
+    }
+})
+
+// for(var i = 0; i < 3; i++){
+//     row.innerHTML = card;
+// }
