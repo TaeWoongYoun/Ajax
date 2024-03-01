d98f92422da9da74256d0a6eea650600015c0cf7
xodnd0507 <24457545yong@gmail.com>
2024-03-01 23:30:27 +0900

    test : True

--- main.js
+++ main.js
@@ -17,21 +17,37 @@
 // .done(function(price){
 //     console.log(price.price)
 // })
-var card = 
-`<div class="col-sm-4">
-<img src="https://via.placeholder.com/600" class="w-100">
-<h5>Card title</h5>
-<p>가격 : 70000</p>
-</div>`
+// var card = 
+// `<div class="col-sm-4">
+// <img src="https://via.placeholder.com/600" class="w-100">
+// <h5>Card title</h5>
+// <p>가격 : 70000</p>
+// </div>`
 
 
-document.querySelectorAll('.row').forEach(function(row){
-    // row.innerHTML = card;
-    for(var i = 0; i < 3; i++){
-        row.innerHTML += card;
-    }
-})
+// document.querySelectorAll('.row').forEach(function(row){
+//     // row.innerHTML = card;
+//     for(var i = 0; i < 3; i++){
+//         row.innerHTML += card;
+//     }
+// })
 
 // for(var i = 0; i < 3; i++){
 //     row.innerHTML = card;
 // }
+
+var products =  [
+    { id : 0, price : 70000, title : 'Blossom Dress' },
+    { id : 1, price : 50000, title : 'Springfield Shirt' },
+    { id : 2, price : 60000, title : 'Black Monastery' }
+];
+
+products.forEach((a, i)=>{
+    var 템플릿 = 
+    `<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${products[i].title}</h5>
+        <p>가격 : ${products[i].price}</p>
+    </div>`;
+    $('.row').append(템플릿)
+});
